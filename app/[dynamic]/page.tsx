7480bc6baed54b17ce2a06efe82f585b47b399fe
Sado4 <derasado04@gmail.com>
2024-09-14 14:33:23 +0900

    debug

--- app/[dynamic]/page.tsx
+++ app/[dynamic]/page.tsx
@@ -1,9 +1,16 @@
+import Client from "@/features/client/components/Client";
+
 export default function Page() {
+  const res = 'fetch';
+  console.log('da');
+  const res2 = 'fetch2';
+  // const window2 = window;
   return (
     <main>
       <button>
         動的なページ
       </button>
+      <Client />
     </main>
   );
 }
---
+++ features/client/components/Client.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export function Client() {
+  const window1 = window;
+  return (
+    <div>
+      <h1>クライアントコンポーネント</h1>
+    </div>
+  );
+}
+
+export default Client;
